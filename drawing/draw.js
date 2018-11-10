5b1deb9361954c2cb0188cb2ab7a413c290ebdc1
Joshua Walker <joshwalk@umich.edu>
2018-11-09 20:40:00 -0500

    updated with proper js syntax

--- drawing/draw.js
+++ drawing/draw.js
@@ -6,7 +6,7 @@ ctx.canvas.height = 500;
 
 document.addEventListener("mousemove", draw);
 document.addEventListener("mouseenter", setPosition);
-document.addEventListener("keydown", draw)
+document.addEventListener("keydown", draw);
 
 // starting position
 var pos = { x: 0, y: 0 };
@@ -17,21 +17,21 @@ function setPosition(e) {
   pos.y = e.clientY;
 }
 
-var size = 5 // default stroke size
-var color = 'blue' // default color
+var size = 5; // default stroke size
+var color = 'blue'; // default color
 
 function draw(e) {
-  if (e.keyCode == 66) { color = 'blue' } // b
-  if (e.keyCode == 71) { color = 'green' } // g
-  if (e.keyCode == 82) { color = 'red' } // r
-  if (e.keyCode == 89) { color = 'yellow' } // y
-  if (e.keyCode == 32) { ctx.clearRect(0,0, 800, 500) } // spacebar
+  if (e.keyCode == 66) { color = 'blue'; } // b
+  if (e.keyCode == 71) { color = 'green'; } // g
+  if (e.keyCode == 82) { color = 'red'; } // r
+  if (e.keyCode == 89) { color = 'yellow'; } // y
+  if (e.keyCode == 32) { ctx.clearRect(0,0, 800, 500); } // spacebar
 
   // up/down, increases/decreases lineWidth
-  if (e.keyCode == 38) { size += 5 }
+  if (e.keyCode == 38) { size += 5; }
   if (e.keyCode == 40) {
     if (size > 5) {
-      size -= 5
+      size -= 5;
     }
   }
 
